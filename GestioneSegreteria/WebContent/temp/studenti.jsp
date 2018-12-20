81704170c987e9894cc5b62f7cc3885b7daf9181
Kristian <reale@dlvsystem.com>
2018-12-21 00:55:13 +0100

    test

---
+++ GestioneSegreteria/WebContent/temp/studenti.jsp
@@ -0,0 +1,73 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:useBean id="user" class="modelTemp.Studente" scope="session" />
+	<jsp:setProperty name="user" property="matricola" value="fin."/>
+
+
+<html>
+<head lang="it">
+<title>Gestione Segreteria Studenti</title>
+<meta charset="utf-8">
+
+<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+<LINK rel="stylesheet" href="css/common.css" type="text/css">
+
+<script src="js/gestioneServizi.js"></script>
+<script src="js/studenti.js"></script>
+
+</head>
+<body>
+
+<header>
+<jsp:getProperty name="user" property="matricola"/>
+<h1>Elenco Studenti</h1>
+<h2>Portale per la gestione degli studenti afferenti a qualche corso di laurea</h2>
+</header>
+
+<div class="table-responsive">
+	<table class="table">
+		<caption>Nominativo degli studenti iscritti</caption>
+		<thead>
+		<tr>
+			<th>Matricola</th>
+			<th>Nome</th>
+			<th>Cognome</th>
+			<th>Data di Nascita</th>
+			<th>Mostra dettagli</th>	
+		</tr>
+		</thead>
+		
+		<tbody id="elencoStudenti">
+		
+		<c:forEach var="studente" items="${studenti}">
+			<tr class="success studente">
+				<td>${studente.matricola}</td>
+				<td>${studente.nome}</td>
+				<td>${studente.cognome}</td>
+				<td><time>${studente.dataNascita}</time></td>
+				<td><input class="mostraDettagli" type="button" value="Mostra"
+					onclick="studente = new Studente('${studente.matricola}', '${studente.nome}', '${studente.cognome}', '${studente.dataNascita}'); dettagli(studente);"></input></td>
+			</tr>			
+		</c:forEach>
+										
+		</tbody>
+		<tfoot>
+		<tr class="active">
+			<td><a href="javascript:onclick=ordinaStudenti('matricola')">Ordina per Matricola</a></td>
+			<td><a href="javascript:onclick=ordinaStudenti('nome')" href="">Ordina per Nome</a></td>
+			<td><a href="javascript:onclick=ordinaStudenti('cognome')" href="">Ordina per Cognome</a></td>
+			<td><a href="javascript:onclick=ordinaStudenti('dataNascita')" href="">Ordina per Data di nascita</a></td>	
+		</tr>
+		</tfoot>		
+	</table>
+	
+	<div id="dettagliStudente"></div>
+</div>
+
+
+</body>
+</html>
---
+++ GestioneSegreteria/srcTemp/controllerTemp/DammiStudenti.java
@@ -0,0 +1,113 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import modelTemp.Studente;
+import persistenceTemp.DatabaseManager;
+import persistenceTemp.dao.StudenteDao;
+
+public class DammiStudenti extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+				
+		
+		StudenteDao dao =DatabaseManager.getInstance()
+			.getDaoFactory().getStudenteDAO();
+		List<Studente> studs = dao.findAll();
+		req.setAttribute("studenti", studs);
+		
+		RequestDispatcher dispacher = 
+				req.getRequestDispatcher
+						("report/studenti.jsp");
+		dispacher.forward(req, resp);
+		
+//		PrintWriter out = resp.getWriter();
+//		out.println("<html>");
+//		out.println("<table border = 1");
+//		out.println("<tr>");
+//		out.println("<th>Matricola</th>");
+//		out.println("<th>Nome</th>");
+//		out.println("<th>Cognome</th>");
+//		out.println("<th>Data di Nascita</th>");
+//		out.println("<th>Indirizzo</th>");
+//		out.println("</tr>");
+//		for (Studente s : dao.findAll()){
+//			out.println("<tr>");
+//			out.println("<td>");
+//			out.println(s.getMatricola());
+//			out.println("</td>");
+//			out.println("<td>");
+//			out.println(s.getNome());
+//			out.println("</td>");
+//			out.println("<td>");
+//			out.println(s.getCognome());
+//			out.println("</td>");
+//			out.println("<td>");
+//			out.println(s.getDataNascita());
+//			out.println("</td>");
+//			out.println("<td>");
+//			out.println(s.getIndirizzo());
+//			out.println("</td>");
+//			out.println("</tr>");
+//		}
+//		out.println("</table>");
+//		out.println("</html>");
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		StringBuffer jsonReceived = new StringBuffer();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream()));		
+		String line = reader.readLine();
+		while (line != null){
+			jsonReceived.append(line);
+			line = reader.readLine();
+		}		
+		System.out.println(jsonReceived.toString());
+		try {
+			JSONObject json = new JSONObject(jsonReceived.toString());				
+			Studente studente = new Studente();
+			studente.setMatricola(json.getString("matricola"));
+			studente.setCognome(json.getString("cognome"));			
+			studente.setNome(json.getString("nome"));
+		
+			StudenteDao dao =DatabaseManager.getInstance()
+					.getDaoFactory().getStudenteDAO();
+			Studente dbStudente  = dao.findByPrimaryKey(studente.getMatricola());
+			
+			JSONObject jsonIndirizzo = new JSONObject(dbStudente.getIndirizzo());
+			
+			PrintWriter out = resp.getWriter();
+			out.println(jsonIndirizzo.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
